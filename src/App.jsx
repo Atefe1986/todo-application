ebe5e3c997896a3c0e591c1d21bd111ec5507832
Atefeh Etemadipour <atefeh.etemadipour@gmail.com>
2025-02-12 10:06:47 +0100

    initialized My todo application

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Link } from "react-router-dom";
+import TodoPage from "./pages/TodoPage";
+import AboutPage from "./pages/AboutPage";
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Todo</Link>
+        <Link to="/about">Om</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<TodoPage />} />
+        <Route path="/about" element={<AboutPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
