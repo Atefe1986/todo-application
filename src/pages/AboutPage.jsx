bf135bc3c3dcb62dfad8bd6d27ad8a6c0607528f
Atefeh Etemadipour <atefeh.etemadipour@gmail.com>
2025-02-13 15:17:02 +0100

    Added P to about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -3,6 +3,7 @@ const AboutPage = () => {
     <div>
       <h1>About Us</h1>
       <p>This app lets you create, manage and complete tasks easily.</p>
+      <p>You can use this application easily.</p>
     </div>
   );
 };
