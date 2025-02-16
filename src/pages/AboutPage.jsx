35acd05c7ba933c6f935315f22f034123743bf97
Atefeh Etemadipour <atefeh.etemadipour@gmail.com>
2025-02-16 18:45:30 +0100

    last changes

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,7 @@ const AboutPage = () => {
       <h1>About Us</h1>
       <p>This app lets you create, manage and complete tasks easily.</p>
       <p>You can use this application easily by watch the video.</p>
-      <video width="100%" controls>
+      <video controls>
         <source src="/tutorial.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
