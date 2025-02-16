d078b5ccc5599d916a9992dd75fea8de901169bf
Atefeh Etemadipour <atefeh.etemadipour@gmail.com>
2025-02-16 17:14:24 +0100

    Added tutorial video in about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,6 +4,10 @@ const AboutPage = () => {
       <h1>About Us</h1>
       <p>This app lets you create, manage and complete tasks easily.</p>
       <p>You can use this application easily by watch the video.</p>
+      <video width="100%" controls>
+        <source src="/tutorial.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
